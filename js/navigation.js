eeef8afea72127b66a9f91bc954a2a43687a8fcc
aanikina <AAnikina@uclan.ac.uk>
2022-03-11 10:02:46 +0200

    added player name change button

--- js/navigation.js
+++ js/navigation.js
@@ -23,24 +23,28 @@ async function populateHeader() {
     let bt_reload = `<a class='app_navi' href='${window.location.href}'>Reload</a>`;
 
     let bt_stop = "";
-    if( PAGE_STATE=='quiz' || PAGE_STATE=='leaderboard' ) {
+    if( PAGE_STATE=='quiz' ) {
         // link to abandon challenge
         // help:
         // https://www.tutorialspoint.com/JavaScript-function-in-href-vs-onClick
-        let link = window.location.href;
         bt_stop=`<a hidden class='app_navi quiz_navi' href='javascript:treasureHuntCancelledEvent()'>Abandon challenge</a>`;
+    }
+    else if( PAGE_STATE=='leaderboard' ) {
+        // link to abandon challenge but with different text
+        bt_stop=`<a hidden class='app_navi quiz_navi' href='javascript:treasureHuntCancelledEvent()'>Another challenge</a>`;
+    }
+
+    let bt_name = "";
+    if( PAGE_STATE=='list' ) {
+        // link to change player name
+        bt_name=`<a hidden class='app_navi quiz_navi' href='javascript:playerNameChangeRequestEvent()'>Change player name</a>`;
     }
 
     // assemble
 
-    header.innerHTML = bt_home + bt_reload + bt_stop;
+    header.innerHTML = bt_home + bt_reload + bt_stop + bt_name;
 
 } // function getChallenges
 
 //-------------------------------------------------------------------------------------------------------------------+++
 // Actual code.
-
-// 2022.02.24
-// switched to `
-// made contents unmanipulateable from outside
-// added PAGE_STATE dependency
